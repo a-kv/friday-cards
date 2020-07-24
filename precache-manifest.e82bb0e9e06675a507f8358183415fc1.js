bc8349f04979bdd12058c2dc623d888c1d48bd51
kvaso <aakvasovaa@gmail.com>
2020-07-24 12:41:52 +0300

    Updates

---
+++ precache-manifest.e82bb0e9e06675a507f8358183415fc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8c6196ea019ea6da7136718ab423504",
+    "url": "/friday-cards/index.html"
+  },
+  {
+    "revision": "980aa61105db5b2ddc3e",
+    "url": "/friday-cards/static/css/main.7fe9d73d.chunk.css"
+  },
+  {
+    "revision": "c24143d77ccc4c91258b",
+    "url": "/friday-cards/static/js/2.a4a47c0a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/friday-cards/static/js/2.a4a47c0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "980aa61105db5b2ddc3e",
+    "url": "/friday-cards/static/js/main.9a6b4c3a.chunk.js"
+  },
+  {
+    "revision": "493d56ac6b5bcb68cfa5",
+    "url": "/friday-cards/static/js/runtime-main.b8aaa407.js"
+  }
+]);
